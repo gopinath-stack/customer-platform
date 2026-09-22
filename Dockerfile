248cf5d08a49396e59e09bb2a25f439fd4e0add1
gopinath-stack <gopinath.manoharan@techfortsoftwareservices.com>
2026-09-22 11:07:49 +0000

    initial customer platform

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+COPY app/requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY app/ .
+
+EXPOSE 8081
+
+CMD ["python", "app.py"]
---
+++ app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask 
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Customer Platform"
+
+@app.route("/health")
+def health():
+    return "healthy"
+
+@app.route("/db")
+def db():
+    conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME", "customerdb"),
+    user=os.getenv("DB_USER", "customer"),
+    password=os.getenv("DB_PASSWORD")
+)
+
+    conn.close()
+    return "Database connection successful"
+
+app.run(host="0.0.0.0", port=8081)
